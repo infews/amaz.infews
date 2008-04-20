5a49d62c85422d5d614e1a662871af8a2fd2d9ca
dwfrank <dwfrank@infe.ws>
2008-04-20 10:21:41 -0700

    cleanup for first push to repo

--- spec/controllers/item_controller_spec.rb
+++ spec/controllers/item_controller_spec.rb
@@ -14,7 +14,6 @@ describe ItemController do
                                                  :asin => '123456'}
   end
 
-  # verify that we call ecs properly in good case
   it 'should call the Amazon library properly when an asin is requested' do
     controller.should_receive(:ecs_item_lookup).with(:asin => '123456').and_return('foo')
     
@@ -23,5 +22,27 @@ describe ItemController do
     assigns[:item].should == 'foo'
   end
   # verify that we call ecs properly in bad cases
+  
+  it 'should retrieve all the required Item fields from the Response 
+      that are needed for a detail view on a Book' do
+    mocked_response_body = File.read('spec/item_lookup_book.xml')
+    AmazonECS::Request.stub!(:fetch).and_return(mocked_response_body)
+
+    get 'index', :asin => '159420120X'
+    
+    item = assigns[:item]
+
+    item.title.should      == 'Against the Day'
+    item.binding.should    == 'Hardcover'
+    item.edition.should    be_nil
+    item.author.should     == 'Thomas Pynchon'
+    item.sales_rank.should == '878'
+    item.list_price.formatted_price.should         == '$35.00'
+    item.offers[0].price.formatted_price.should    == '$7.00'
+    item.customer_reviews.average_rating.should    == '4.5'
+    item.customer_reviews.number_of_reviews.should == '38'
+    # TODO: image
+    # TODO: description
+  end
   
 end
