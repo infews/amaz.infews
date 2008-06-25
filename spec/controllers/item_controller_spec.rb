b338016d6c06843f2f1f9acf45d9ed6393572e28
Davis W. Frank <dwfrank+github@infe.ws>
2008-06-24 18:19:18 -0700

    dwf: refactored ItemControllerSpec to stub at the interface to AmazonAWS instead of at the http call

--- spec/controllers/item_controller_spec.rb
+++ spec/controllers/item_controller_spec.rb
@@ -2,84 +2,103 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 # TODO: stub out the calls to the library instead of stubbing out fetch
 
-describe ItemController, '#show' do
-  before :each do
-    class << controller
-      public(:aws_request)
+describe ItemController do
+
+  describe '#show' do
+
+    it 'should call Amazon with an ItemLookup properly when an asin is requested and instantiate an ItemPresenter' do
+      item = mock('item_presenter', {:asin => '0143112562'})
+      ItemPresenter.stub!(:new).and_return(item)
+      aws_response = mock('aws_response', {:items => [item]})
+      controller.should_receive(:aws_item_lookup).with({:asin => '0143112562'}).once.and_return(aws_response)
+
+      get 'show', :asin => '0143112562'
+
+      assigns[:item].asin.should == '0143112562'
     end
-  end
-
-  it 'should call Amazon with an ItemLookup properly when an asin is requested and instantiate an ItemPresenter' do
-    response_xml = File.read('spec/response_xml/item_lookup_book.xml')
-    controller.aws_request.stub!(:fetch).and_return(response_xml)
-    
-    get 'show', :asin => '0143112562'
-    
-    assigns[:item].asin.should == '0143112562'
-  end
   
-  it 'should not instantiate an ItemPresenter if an item is not found' do
-    response_xml = File.read('spec/response_xml/item_lookup_invalid.xml')
-    controller.aws_request.stub!(:fetch).and_return(response_xml)
-    
-    get 'show', :asin => 'fffffffff'
-    
-    assigns[:item].should be_nil
-  end
-end
+    it 'should not instantiate an ItemPresenter if an item is not found' do
+      aws_response = mock('aws_response', {:items => []})
+      controller.should_receive(:aws_item_lookup).with({:asin => 'fffffffff'}).once.and_return(aws_response)
 
-describe ItemController, '#search' do
-  before :each do
-    class << controller
-      public(:aws_request)
+      get 'show', :asin => 'fffffffff'
+
+      assigns[:item].should be_nil
     end
-  end
-
-  it 'should call Amazon with a book keyword search' do
-    response_xml = File.read('spec/response_xml/item_search_book_keyword.xml')
-    controller.aws_request.stub!(:fetch).and_return(response_xml)
     
-    get 'search', :search_index => 'Books', 
-                  :keywords => 'Against the day', 
-                  :page => '1'
-    
-    assigns[:results].should_not be_nil   
-    assigns[:results].items.size.should == 10
   end
   
-  it 'should support searching to an arbitrary page of search results' do
-    response_xml = File.read('spec/response_xml/item_search_book_page_last.xml')
-    controller.aws_request.stub!(:fetch).and_return(response_xml)
-    
-    get 'search', :search_index => 'Books', 
-                  :keywords => 'harry potter', 
-                  :page => '178'
+  describe '#search' do
 
-    assigns[:results].should_not be_nil   
-    assigns[:results].items.size.should == 1    
-  end
+    it 'should call Amazon with a book keyword search' do
+      search_options = {:keywords => 'Against the Day',
+                        :page => '1',
+                        :search_index => 'Books'}
+      controller.should_receive(:aws_item_search).with(search_options).once.and_return('aws_response')
+      items = mock('item_presenters_array', :size => 4)
+      search = mock('search_results_presenter', {:items => items})
+      SearchResultsPresenter.stub!(:new).and_return(search)
 
-  it 'should preserve the state of the previous seach' do
-    response_xml = File.read('spec/response_xml/item_search_book_page_last.xml')
-    controller.aws_request.stub!(:fetch).and_return(response_xml)
+      get 'search', :search_index => 'Books', 
+                    :keywords => 'Against the Day', 
+                    :page => '1'
 
-    get 'search', :search_index => 'Books', 
-                  :keywords => 'Against the day', 
-                  :page => '1'
-
-    assigns[:previous_keywords].should == 'Against the day'
-    assigns[:previous_search_index].should == 'Books'    
-  end
-
-  it 'should support a search for book bestsellers' do
-    response_xml = File.read('spec/response_xml/item_search_book_bestsellers.xml')
-    controller.aws_request.stub!(:fetch).and_return(response_xml)
-    get 'search', :search_index => 'Books', 
-                  :bestsellers => 'true'
-    assigns[:previous_search_index].should == 'Books'    
-  end
-   
-  it 'should tell the user that the item is not found'     
-  it 'should redirect to an empty search page when an item is not found'
+      assigns[:results].should_not be_nil   
+      assigns[:results].items.size.should == 4
+    end
   
-end
+    it 'should support searching to an arbitrary page of search results' do
+      search_options = {:search_index => 'Books', 
+                        :keywords => 'harry potter', 
+                        :page => '178'}
+      controller.should_receive(:aws_item_search).with(search_options).once.and_return('aws_response')
+      items = mock('item_presenters_array', :size => 1)
+      search = mock('search_results_presenter', {:items => items})
+      SearchResultsPresenter.stub!(:new).and_return(search)
+
+      get 'search', :search_index => 'Books', 
+                    :keywords => 'harry potter', 
+                    :page => '178'
+
+      assigns[:results].should_not be_nil   
+      assigns[:results].items.size.should == 1    
+    end
+
+    it 'should preserve the state of the previous seach' do
+      search_options = {:keywords => 'Against the Day',
+                        :page => '1',
+                        :search_index => 'Books'}
+      controller.should_receive(:aws_item_search).with(search_options).once.and_return('aws_response')
+      items = mock('item_presenters_array', :size => 4)
+      search = mock('search_results_presenter', {:items => items})
+      SearchResultsPresenter.stub!(:new).and_return(search)
+
+      get 'search', :search_index => 'Books', 
+                    :keywords => 'Against the Day', 
+                    :page => '1'
+
+      assigns[:previous_keywords].should == 'Against the Day'
+      assigns[:previous_search_index].should == 'Books'    
+    end
+
+    it 'should support a search for book bestsellers' do
+      search_options = {:page => '1',
+                        :browse_node => '1000',
+                        :sort => 'salesrank',
+                        :search_index => 'Books'}
+      controller.should_receive(:aws_item_search).with(search_options).once.and_return('aws_response')
+      items = mock('item_presenters_array', :size => 4)
+      search = mock('search_results_presenter', {:items => items})
+      SearchResultsPresenter.stub!(:new).and_return(search)
+
+      get 'search', :search_index => 'Books', 
+                    :bestsellers => 'true'
+      assigns[:previous_search_index].should == 'Books'    
+    end
+
+    it 'should tell the user that the item is not found'     
+    it 'should redirect to an empty search page when an item is not found'
+
+  end
+
+end  
