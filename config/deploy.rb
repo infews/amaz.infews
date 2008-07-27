48d6ed8010a4bcb01a2a61efed791808125353fa
Davis W. Frank <dwfrank+github@infe.ws>
2008-07-27 13:38:53 -0700

    dwf: tweaks to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :repository,  'git@github.com:infews/amaz.infews.git'
 set :scm, "git"
 set :scm_passphrase, 'scotch4code' #This is your custom users password
 set :user, 'infews'
+set :use_sudo, true
 
 role :app, "infews-prod1"
 role :web, "infews-prod1"
