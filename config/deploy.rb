35885131005efbcf1fbb865b3a6765758a8a583b
Davis W. Frank <dwfrank+github@infe.ws>
2008-07-28 22:34:36 -0700

    dwf: whoops! cap now knows about mongrel_cluster

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+require 'mongrel_cluster/recipes'
 default_run_options[:pty] = true
 
 set :application, 'amaz.infews'
@@ -10,3 +11,5 @@ set :use_sudo, true
 role :app, "infews-prod1"
 role :web, "infews-prod1"
 role :db,  "infews-prod1", :primary => true
+
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
