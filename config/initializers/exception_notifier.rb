c96b31fc90441bbb85996a0c213c25eb540f02ba
Davis W. Frank <dwfrank+github@infe.ws>
2008-08-07 23:12:15 -0700

    dwf: tweaking haml version in config.gems

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -2,9 +2,9 @@ require 'smtp_tls'
 
 ExceptionNotifier.exception_recipients = %w(crash@infe.ws)
 
-ActionMailer::Base.server_settings = { :address => "smtp.gmail.com",
+ActionMailer::Base.server_settings = { :address => 'smtp.gmail.com',
                                        :port => "587",
                                        :authentication => :plain,
                                        :domain => 'infe.ws',
-                                       :user_name => "infews@infe.ws",
-                                       :password => "scotch4code"}
+                                       :user_name => 'infews@infe.ws',
+                                       :password => 'scotch4code'}
