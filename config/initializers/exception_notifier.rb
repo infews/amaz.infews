3f213c0d13fe0e910c99234c78be7caaf3c27294
Davis W. Frank <dwfrank+github@infe.ws>
2008-08-07 23:24:56 -0700

    dwf: renamed method thanks to deprecated ActionMailer settings

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -2,10 +2,10 @@ require 'smtp_tls'
 
 ExceptionNotifier.exception_recipients = %w(crash@infe.ws)
 
-ActionMailer::Base.server_settings = { :address => 'smtp.gmail.com',
-                                       :port => "587",
-                                       :tls => true,
-                                       :authentication => :plain,
-                                       :domain => 'infe.ws',
-                                       :user_name => 'infews@infe.ws',
-                                       :password => 'scotch4code'}
+ActionMailer::Base.smtp_settings = { :address => 'smtp.gmail.com',
+                                     :port => "587",
+                                     :tls => true,
+                                     :authentication => :plain,
+                                     :domain => 'infe.ws',
+                                     :user_name => 'infews@infe.ws',
+                                     :password => 'scotch4code' }
