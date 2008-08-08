a15846a0bf02fe33e882d8981b10a8cf3bfd95b8
Davis W. Frank <dwfrank+github@infe.ws>
2008-08-07 23:39:29 -0700

    dwF: MORE GMAIL

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -4,6 +4,7 @@ ExceptionNotifier.exception_recipients = %w(dwfrank@infe.ws)
 
 ActionMailer::Base.smtp_settings = { :address => 'smtp.gmail.com',
                                      :port => "587",
+                                     :tls => true,
                                      :authentication => :plain,
                                      :domain => 'infe.ws',
                                      :user_name => 'infews@infe.ws',
