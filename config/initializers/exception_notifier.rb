dd3f5723b22c2c01cba20d2d1e3bfcd92b23d6f0
Davis W. Frank <dwfrank+github@infe.ws>
2008-08-07 23:15:44 -0700

    dwf: more gmail smtp tweaking

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -4,6 +4,7 @@ ExceptionNotifier.exception_recipients = %w(crash@infe.ws)
 
 ActionMailer::Base.server_settings = { :address => 'smtp.gmail.com',
                                        :port => "587",
+                                       :tls => true,
                                        :authentication => :plain,
                                        :domain => 'infe.ws',
                                        :user_name => 'infews@infe.ws',
