5ee1b88ba35f2b447ea98ad0f69179be5fbc6105
Davis W. Frank <dwfrank+github@infe.ws>
2008-08-07 23:00:15 -0700

    dwf: tweaking smtp settings to see if we can connect to gmail

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -5,5 +5,6 @@ ExceptionNotifier.exception_recipients = %w(crash@infe.ws)
 ActionMailer::Base.server_settings = { :address => "smtp.gmail.com",
                                        :port => "587",
                                        :authentication => :plain,
+                                       :domain => 'localhost.localdomain',
                                        :user_name => "infews@infe.ws",
                                        :password => "scotch4code"}
